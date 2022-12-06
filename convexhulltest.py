1bf68283a5c8f56faf23105319bdf0b7be711dc9
Lau Yan Han <sps08.lauyanhan@gmail.com>
2022-12-06 15:05:36 +0800

    feat: Finish convex hull Re-ID algorithm
    Pending Re-ID results visualisation and tests

--- convexhulltest.py
+++ convexhulltest.py
@@ -40,7 +40,8 @@ def extract_data(src):
             extracted.append(row)
     return extracted
 
-# return intersection of two 2d lines in x-y coordinates
+# return intersection of two 2d lines in x-y coordinates (ndarray format)
+# input: endpoints of both lines (x-y ndarray format)
 # https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
 def two_lines_intersection(line1_ptA, line1_ptB, line2_ptA, line2_ptB):
     x_1A = line1_ptA[0]
@@ -66,17 +67,18 @@ def two_lines_intersection(line1_ptA, line1_ptB, line2_ptA, line2_ptB):
     if det:
         x = (b2*c1 - b1*c2)/det
         y = (a1*c2 - a2*c1)/det
-        return x,y
     else:
         # lines are parallel if determinant = 0
-        return None, None
+        x = 1e10
+        y = 1e10
+    return np.array([x, y])
 
 # return cross ratio of 4 collinear 2D points (represented as arrays)
 def cross_ratio(a, b, c, d):
-    ac = np.linalg.norm(a,c)
-    bd = np.linalg.norm(b,d)
-    bc = np.linalg.norm(b,c)
-    ad = np.linalg.norm(a,d)
+    ac = np.linalg.norm(np.subtract(a,c))
+    bd = np.linalg.norm(np.subtract(b,d))
+    bc = np.linalg.norm(np.subtract(b,c))
+    ad = np.linalg.norm(np.subtract(a,d))
     return (ac*bd)/(bc*ad)
 
 class camera:
@@ -94,9 +96,6 @@ class camera:
         self.areas = np.ones(self.num_of_targets)
         self.centroids = np.ones([self.num_of_targets,2])
 
-        # topological data
-        self.hull = []
-    
     # assign target IDs and extract areas + XY centroids from CSV data
     # convert from image convention (origin at top left)
     # to standard XY convention (origin at bottom left)
@@ -112,25 +111,51 @@ class camera:
             self.centroids[id][1] = cen_y
             id += 1
     
+    # generate convex hull of detected targets and initialise list of cross ratios
     def generate_convexhull(self):
         self.hull = ConvexHull(self.centroids)
-        if len(self.hull.vertices) < 5:
+        self.hull_nvertices = len(self.hull.vertices)
+        if self.hull_nvertices < 5:
             raise Exception("Algorithm needs 5 or more convex hull vertices")
-        print(self.hull.simplices)
-        print(self.hull.vertices)
+        self.cross_ratio_seq = np.zeros(self.hull_nvertices)
     
-    # Given a convex hull vertex's position, get the target ID of the vertex and its 4 neighbours
-    def get_vertice_and_neighbour_IDs(self, hull_pos):
-        hull_nvertices = len(self.hull.vertices)
-        if hull_pos >= hull_nvertices:
+    # Given a convex hull vertex's position, get the target ID and centroids of the vertex and its 4 neighbours
+    def get_vertex_and_neighbour_data(self, hull_pos):
+        if hull_pos >= self.hull_nvertices:
             raise Exception("Position exceeds number of convex hull vertices")
+        # neighbours: Previous two vertices, and next two vertices
         positions = np.array([hull_pos - 2, hull_pos - 1, hull_pos, hull_pos + 1, hull_pos + 2])
         # wraparound the number of convex hull vertices
-        positions = np.mod(positions, hull_nvertices)
+        positions = np.mod(positions, self.hull_nvertices)
+        # given the positions, recover the original target IDs and x-y centroids of these vertices
         target_IDs = np.zeros(5, dtype=int)
+        target_centroids = np.zeros([5,2])
         for i in range(5):
             target_IDs[i] = self.hull.vertices[positions[i]]
-        return target_IDs
+            target_centroids[i] = self.centroids[target_IDs[i]]  
+        return target_IDs, target_centroids
+    
+    # calculate the cross ratio for a given vertex's position on the convex hull
+    def cross_ratio_single_vertex(self, hull_pos):
+        _, target_centroids = self.get_vertex_and_neighbour_data(hull_pos)
+        # get key points
+        pt1 = target_centroids[1]
+        pt2 = two_lines_intersection(target_centroids[0], target_centroids[2],
+                                    target_centroids[1], target_centroids[3])
+        pt3 = two_lines_intersection(target_centroids[1], target_centroids[3],
+                                    target_centroids[2], target_centroids[4])
+        pt4 = target_centroids[3]
+        return cross_ratio(pt1, pt2, pt3, pt4)
+    
+    # calculate cross ratio for all vertices on the convex hull
+    def cal_cross_ratio_seq(self):
+        for hull_pos in range(self.hull_nvertices):
+            self.cross_ratio_seq[hull_pos] = self.cross_ratio_single_vertex(hull_pos)
+    
+    # takes a convex vertex position (pos) and returns cross ratios of the remaining sequence
+    # in the following order: pos+1, pos+2 ... and wraparound to 0, 1, ... pos-1
+    def get_remaining_seq_cross_ratios(self, hull_pos):
+        return np.concatenate((self.cross_ratio_seq[hull_pos+1:], self.cross_ratio_seq[:hull_pos]))
 
 fig = plt.figure()
 ax = fig.add_subplot()
@@ -139,8 +164,11 @@ ax.set_ylabel('Y')
 
 cameras = (camera(0, csv_files[0]), camera(1, csv_files[1]))
 for cam in cameras:
+    # generate initial data
     cam.get_target_areas_centroids()
     cam.generate_convexhull()
+    cam.cal_cross_ratio_seq()
+    # visualise convex hulls
     if cam.cam_ID:
         m = 'o'
     else:
@@ -148,5 +176,17 @@ for cam in cameras:
     plt.scatter(cam.centroids[:,0], cam.centroids[:,1], marker=m)
     for simplex in cam.hull.simplices:
         plt.plot(cam.centroids[simplex,0], cam.centroids[simplex,1], 'k-')
+
+# associate across cameras
+assoc_err = 1e5*np.ones([cameras[0].num_of_targets, cameras[1].num_of_targets])
+for pos_0, ID_0 in enumerate(cameras[0].hull.vertices):
+    cross_ratio_seq_0 = cameras[0].get_remaining_seq_cross_ratios(pos_0)
+    for pos_1, ID_1 in enumerate(cameras[1].hull.vertices):
+        cross_ratio_seq_1 = cameras[1].get_remaining_seq_cross_ratios(pos_1)
+        cross_ratio_err = np.abs(np.subtract(cross_ratio_seq_1, cross_ratio_seq_0))
+        assoc_err[ID_0][ID_1] = np.mean(cross_ratio_err)
+
+# to-do: Display Re-ID results
+print(assoc_err)
 
 plt.show()
